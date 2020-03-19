429c90ccaa8ea6e47e39e2fb518ee2ba7774be41
hjh60 <h3nry123>
2020-03-19 23:25:21 +1300

    started on petitions functionality

--- app/controllers/petitons.controller.js
+++ app/controllers/petitons.controller.js
@@ -1,17 +1,59 @@
 const petitions = require('../models/petition.models');
 
 exports.lister = async function( req, res ) {
-    console.log("Here")
-    const start_index = req.params.startIndex;
-    const count = req.params.count;
-    const q = req.params.q;
-    const categoryId = req.params.categoryId;
-    const authorId = req.params.authorId;
-    const sortBy = req.params.sortBy;
+    const q = req.query.q;
+    const categoryId = req.query.categoryId;
+    const authorId = req.query.authorId;
+    const sortBy = req.query.sortBy;
+    let count = (req.query.count);
+    let startIndex = (req.query.startIndex);
+    if (authorId != null) {
+        if (isNaN(authorId)) {
+            res.status(400)
+                .send();
+        }
+    }
+
+    if (startIndex != null) {
+        if (isNaN(startIndex) == true) {
+            res.status(400)
+                .send();
+        }
+    }
+    if (count != null) {
+        if (isNaN(count) == true) {
+            res.status(400)
+                .send();
+        }
+    }
+    count = parseInt(count);
+    startIndex = parseInt(startIndex);
+    if (isNaN(startIndex) == true) {
+        startIndex = 0;
+    }
+    if (categoryId != null) {
+        if (categoryId > 7 || categoryId < 0 || categoryId.length == 0 || isNaN(categoryId)) {
+            res.status(400)
+                .send();
+        }
+    }
+    if (sortBy != null) {
+        if (sortBy != "ALPHABETICAL_ASC" && sortBy != "ALPHABETICAL_DESC" && sortBy != "SIGNATURES_ASC" && sortBy != "SIGNATURES_DESC") {
+            res.status(400)
+                .send();
+        }
+    }
+    const endIndex = startIndex + count;
     try {
         const result = await petitions.list(q, categoryId, authorId, sortBy);
-        res.status( 200 )
-            .send(result);
+        if (isNaN(count) == false) {
+            res.status(200)
+                .send(result.slice(startIndex, endIndex));
+        }
+        else if (isNaN(count) == true) {
+            res.status(200)
+                .send(result.slice(startIndex));
+        }
     } catch( err ) {
         res.status( 500 )
             .send( `ERROR getting users ${ err }` );
@@ -22,7 +64,21 @@ exports.create = async function(req, res){
     return null;
 };
 exports.read = async function(req, res){
-    return null;
+    const pet_id = req.params.id;
+    try {
+        const result = await petitions.read(pet_id);
+        if (result.length != 0) {
+            res.status(200)
+                .send(result[0]);
+        }
+        if (result.length == 0) {
+            res.status(404)
+                .send("No result");
+        }
+    } catch (err) {
+        res.status(500)
+            .send("Internal Server Error");
+    }
 };
 exports.update = async function(req, res){
     return null;
--- app/models/petition.models.js
+++ app/models/petition.models.js
@@ -2,26 +2,60 @@ const db = require('../../config/db');
 
 
 exports.list = async function(q, categoryId, authorId, sortBy) {
-    console.log( 'Request to get all petitions from the database...' );
+    if (sortBy == "ALPHABETICAL_ASC") {
+        sortBy = ' order by title asc';
+    }
+    else if (sortBy == "ALPHABETICAL_DESC") {
+        sortBy = ' order by title desc';
+    }
+    else if (sortBy == "SIGNATURES_ASC") {
+        sortBy = ' order by signatureCount asc';
+    }
+    else {
+        sortBy = ' order by signatureCount desc';
+    }
+    if (q == null) {
+        q = "%%";
+    }
+    else if (q != null) {
+        q = "%" + q + "%";
+    }
+    if (authorId == null) {
+        authorId = "%%";
+    }
+    else if (authorId != null) {
+        authorId = "%" + authorId;
+    }
+    if (categoryId == null) {
+        categoryId = "%%";
+    }
+    else if (categoryId != null) {
+        categoryId = "%" + categoryId + "%";
+    }
     const conn = await db.getPool().getConnection();
-    const query = 'select Signature.petition_id as petitionId, title, Category.name as category, User.name as authorName, count(Signature.petition_id) as signatureCount from Signature JOIN Petition on Signature.petition_id = Petition.petition_id JOIN Category on Petition.category_id = Category.category_id JOIN User on Petition.author_id = User.user_id group by Signature.petition_id order by signatureCount desc;'
-    const [ rows ] = await conn.query( query );
+    const query = "select Signature.petition_id as petitionId, title, Category.name as category, User.name as authorName, count(Signature.petition_id) as signatureCount from Signature JOIN Petition on Signature.petition_id = Petition.petition_id JOIN Category on Petition.category_id = Category.category_id JOIN User on Petition.author_id = User.user_id where Petition.title LIKE ? and Petition.author_id LIKE ? and Petition.category_id LIKE ? group by Signature.petition_id" + sortBy;
+    const [ rows ] = await conn.query( query, [q, authorId, categoryId]);
     conn.release();
     return rows;
 };
 
-exports.getOne = async function(){
-    return null;
-};
-
 exports.insert = async function( petition ) {
-    console.log( `Request to insert ${pettion} into the database...` );
     const conn = await db.getPool().getConnection();
     const query = 'insert into Petition (pettion) values ( ? )';
     const [ result ] = await conn.query( query, [ pettion ] );
     conn.release();
     return result;
 };
+
+exports.read = async function(pet_id) {
+    const conn = await db.getPool().getConnection();
+    const query = 'select Petition.petition_id as petitionId, Petition.title, Petition.description, Petition.author_id as authorId, User.name as authorName, User.city as authorCity, User.country as authorCountry, count(Signature.petition_id) as signatureCount, Category.name as category, Petition.created_date as createdDate, Petition.closing_date as closingDate\n' +
+        'from Signature JOIN Petition on Signature.petition_id = Petition.petition_id JOIN User on User.user_id = Petition.author_id JOIN Category on Category.category_id = Petition.category_id\n' +
+        'WHERE Petition.petition_id = ? group by Signature.petition_id';
+    const result = await conn.query(query, [pet_id]);
+    conn.release();
+    return result[0];
+}
 
 exports.alter = async function(){
     return null;
