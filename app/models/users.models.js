472bc0a9216a1344962bfa650103421a32420a26
hjh60 <hjh60@uclive.ac.nz>
2020-04-01 17:20:44 +1300

    added checker if auth token on database is null

--- app/models/users.models.js
+++ app/models/users.models.js
@@ -178,6 +178,12 @@ exports.removePhoto = async function(userId, auth_token) {
         conn.release();
         return 404;
     }
+    const auth_token_check = 'select auth_token from User where user_id = ?';
+    const auth_result = await conn.query(auth_token_check, [userId]);
+    if (auth_result[0][0].auth_token == null) {
+        conn.release();
+        return(401);
+    }
     const correct_user = 'select user_id from User where auth_token = ?';
     const user_result = await conn.query(correct_user, [auth_token]);
     if (user_result[0].length == 0) {
