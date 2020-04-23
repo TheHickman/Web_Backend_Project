faf281ddec20599327c6c37071e853da562e21ba
hjh60 <hjh60@uclive.ac.nz>
2020-04-23 13:38:18 +1200

    removed my conns

--- app/models/petition.models.js
+++ app/models/petition.models.js
@@ -35,22 +35,19 @@ exports.list = async function(q, categoryId, authorId, sortBy) {
     const conn = await db.getPool().getConnection();
     const query = "select Signature.petition_id as petitionId, title, Category.name as category, User.name as authorName, count(Signature.petition_id) as signatureCount from Signature JOIN Petition on Signature.petition_id = Petition.petition_id JOIN Category on Petition.category_id = Category.category_id JOIN User on Petition.author_id = User.user_id where Petition.title LIKE ? and Petition.author_id LIKE ? and Petition.category_id LIKE ? group by Signature.petition_id" + sortBy;
     const [ rows ] = await conn.query( query, [q, authorId, categoryId]);
-    conn.release();
     return rows;
 };
 
 exports.insert = async function(auth_token, title, description, categoryId, createdDate, closingDate) {
-    const conn = await db.getPool().getConnection();
+    const conn = await db.getPool();
     const query = 'select user_id from User where auth_token = ?';
     const user_table = await conn.query(query, [auth_token] );
     if (user_table[0].length == 0) {
-        conn.release();
         return 401;
 
     }
     const insertion = "insert into Petition (author_id, category_id, closing_date, created_date, description, title)  VALUES (?, ?, ?, ?, ?, ?)";
     const result = await conn.query(insertion, [user_table[0][0].user_id, categoryId, closingDate, createdDate, description, title]);
-    conn.release();
     return result[0].insertId;
 };
 
@@ -58,33 +55,29 @@ exports.insert = async function(auth_token, title, description, categoryId, crea
 
 
 exports.read = async function(pet_id) {
-    const conn = await db.getPool().getConnection();
+    const conn = await db.getPool();
     const query = 'select Petition.petition_id as petitionId, Petition.title, Petition.description, Petition.author_id as authorId, User.name as authorName, User.city as authorCity, User.country as authorCountry, count(Signature.petition_id) as signatureCount, Category.name as category, Petition.created_date as createdDate, Petition.closing_date as closingDate\n' +
         'from Signature JOIN Petition on Signature.petition_id = Petition.petition_id JOIN User on User.user_id = Petition.author_id JOIN Category on Category.category_id = Petition.category_id\n' +
         'WHERE Petition.petition_id = ? group by Signature.petition_id';
     const result = await conn.query(query, [pet_id]);
-    conn.release();
     return result[0];
 }
 
 
 
 exports.alter = async function(pet_id, auth_token, title, description, categoryId, closingDate){
-    const conn = await db.getPool().getConnection();
+    const conn = await db.getPool();
     const user = 'select user_id from User where auth_token = ?';
     const is_logged = await conn.query(user, [auth_token])
     if (is_logged[0].length == 0) {
-        conn.release();
         return 401;
     }
     const is_petition = "select author_id from Petition where petition_id = ?";
     const belongs = await conn.query(is_petition, [pet_id]);
     if (belongs[0].length == 0) {
-        conn.release();
         return 404;
     }
     if (is_logged[0][0].user_id != belongs[0][0].author_id) {
-        conn.release();
         return 403;
     }
     const test = 'select * from Petition where petition_id = ?';
@@ -96,60 +89,51 @@ exports.alter = async function(pet_id, auth_token, title, description, categoryI
     let now = new Date();
     if (old_closing_date != null) {
         if (old_closing_date <= now) {
-            conn.release();
             return 400;
         }
     }
     const up_dog = 'update Petition set title = IfNull(?, ?), category_id = IfNull(?, ?), description = IfNull(?, ?), closing_date = IfNull(?, ?) where petition_id = ?';
     const updated = await conn.query(up_dog, [title, old_title, categoryId, old_category, description, old_description, closingDate, old_closing_date, pet_id]);
-    conn.release();
 };
 
 exports.remove = async function(pet_id, auth_token){
-    const conn = await db.getPool().getConnection();
+    const conn = await db.getPool();
     const is_u = 'select user_id from User where auth_token = ?';
     const is_logged = await conn.query(is_u, [auth_token])
     if (is_logged[0].length == 0) {
-        conn.release();
         return 401;
     }
     const is_petition = "select author_id from Petition where petition_id = ?";
     const belongs = await conn.query(is_petition, [pet_id]);
     if (belongs[0].length == 0) {
-        conn.release();
         return 404;
     }
     if (is_logged[0][0].user_id != belongs[0][0].author_id) {
-        conn.release();
         return 403;
     }
     const query = 'DELETE from Petition where petition_id = ?';
     const result = await conn.query(query, [pet_id]);
-    conn.release();
 };
 
 exports.listcats = async function() {
-    const conn = await db.getPool().getConnection();
+    const conn = await db.getPool();
     const query = 'select category_id as categoryId, name from Category';
     const result = await conn.query(query);
-    conn.release();
     return result[0];
 };
 
 exports.getSigs = async function(pet_id) {
-    const conn = await db.getPool().getConnection();
+    const conn = await db.getPool();
     const query = "select signatory_id as signatoryId, User.name as name, User.city as city, User.country as country, signed_date as signedDate from Signature JOIN User on Signature.signatory_id = User.user_id where petition_id = ? order by signedDate asc";
     const result = await conn.query(query, [pet_id]);
-    conn.release();
     return result[0];
 };
 
 exports.postSigs = async function(auth_token, pet_id) {
-    const conn = await db.getPool().getConnection();
+    const conn = await db.getPool();
     const exists = 'select * from Petition where petition_id = ?';
     const does_it = await conn.query(exists, [pet_id]);
     if (does_it[0].length == 0) {
-        conn.release();
         return 404;
     }
     const closed = 'select closing_date from Petition where petition_id = ?';
@@ -157,53 +141,45 @@ exports.postSigs = async function(auth_token, pet_id) {
     const closing_date = is_closed[0][0].closing_date;
     let now = new Date();
     if (closing_date <= now) {
-        conn.release();
         return 403
     }
     const is_logged = 'select user_id from User where auth_token = ?';
     const user_table = await conn.query(is_logged, [auth_token]);
     if (user_table[0].length == 0) {
-        conn.release();
         return 401;
     }
     const user_id = user_table[0][0].user_id;
     const has_signed = 'select signatory_id from Signature where signatory_id = ? and petition_id = ?';
     const have_you = await conn.query(has_signed, [user_id, pet_id]);
     if (have_you[0].length != 0) {
-        conn.release();
         return 403;
     }
     const insertion = 'insert into Signature (petition_id, signatory_id, signed_date) VALUES (?, ?, ?)';
     const has_inserted = await conn.query(insertion, [pet_id, user_id, now]);
-    conn.release();
     return true;
 };
 
 exports.removeSigs = async function(auth_token, pet_id) {
-    const conn = await db.getPool().getConnection();
+    const conn = await db.getPool();
     const exists = 'select * from Petition where petition_id = ?';
     const does_it = await conn.query(exists, [pet_id]);
     if (does_it[0].length == 0) {
-        conn.release();
         return 404;
     }
     const is_logged = 'select user_id from User where auth_token = ?';
     const user_table = await conn.query(is_logged, [auth_token]);
     if (user_table[0].length == 0) {
-        conn.release();
         return 401;
     }
     const user_id = user_table[0][0].user_id;
     const is_author = 'select author_id, petition_id from Petition where author_id = ? and petition_id = ?';
     const authored = await conn.query(is_author, [user_id, pet_id]);
     if (authored[0].length != 0) {
-        conn.release();
         return 403;
     }
     const has_signed = 'select signatory_id from Signature where signatory_id = ? and petition_id = ?';
     const have_you = await conn.query(has_signed, [user_id, pet_id]);
     if (have_you[0].length == 0) {
-        conn.release();
         return 403;
     }
     const closed = 'select closing_date from Petition where petition_id = ?';
@@ -211,47 +187,40 @@ exports.removeSigs = async function(auth_token, pet_id) {
     const closing_date = is_closed[0][0].closing_date;
     let now = new Date();
     if (closing_date <= now) {
-        conn.release();
         return 403
     }
     const deletion = 'delete from Signature where signatory_id = ? and petition_id = ?';
     const is_delete = await conn.query(deletion, [user_id, pet_id]);
-    conn.release();
     return 200;
 };
 
 exports.getPhoto = async function(pet_id) {
-    const conn = await db.getPool().getConnection();
+    const conn = await db.getPool();
     const query = 'select photo_filename from Petition where petition_id = ?';
     const result = await conn.query(query, [pet_id]);
     if (result[0].length == 0) {
-        conn.release();
         return 404;
     }
     const filename = result[0][0].photo_filename;
-    conn.release();
     return filename;
 };
 
 exports.postPhoto = async function(auth_token, pet_id, file_name) {
-    const conn = await db.getPool().getConnection();
+    const conn = await db.getPool();
     const exists = 'select * from Petition where petition_id = ?';
     const does_it = await conn.query(exists, [pet_id]);
     if (does_it[0].length == 0) {
-        conn.release();
         return 404;
     }
     const is_logged = 'select user_id from User where auth_token = ?';
     const user_table = await conn.query(is_logged, [auth_token]);
     if (user_table[0].length == 0) {
-        conn.release();
         return 401;
     }
     const user_id = user_table[0][0].user_id;
     const is_author = 'select author_id, petition_id from Petition where author_id = ? and petition_id = ?';
     const authored = await conn.query(is_author, [user_id, pet_id]);
     if (authored[0].length == 0) {
-        conn.release();
         return 403;
     }
     const photo_exists = 'select photo_filename from Petition where petition_id = ?';
@@ -260,14 +229,11 @@ exports.postPhoto = async function(auth_token, pet_id, file_name) {
         console.log('here');
         const replace = 'update Petition set photo_filename = ? where petition_id = ?';
         const replaced = await conn.query(replace, [file_name, pet_id])
-        conn.release();
         return 201;
     }
     else {
         const create = 'update Petition set photo_filename = ? where petition_id = ?';
         const created = await conn.query(create, [file_name, pet_id])
-        conn.release();
         return 200;
     }
-    conn.release();
 }
--- app/models/users.models.js
+++ app/models/users.models.js
@@ -7,11 +7,10 @@ exports.hash = async function (password) {
 }
 
 exports.register= async function( name, email, password, city, country ) {
-    const conn = await db.getPool().getConnection();
+    const conn = await db.getPool();
     const test = 'select * from User where email = ?';
     const test_result = await conn.query(test, [email]);
     if (test_result[0].length != 0) {
-        conn.release();
         return 400;
     }
     else {
@@ -19,47 +18,40 @@ exports.register= async function( name, email, password, city, country ) {
         console.log(hashed_man);
         const query = 'insert into User (name, email, password, city, country ) values ( ?, ?, ?, ?, ? )';
         const [result] = await conn.query(query, [name, email, hashed_man, city, country]);
-        conn.release();
         return result.insertId;
     }
-    conn.release();
 };
 
 exports.login = async function(email, password){
     const token = crypto.randomBytes(16).toString('hex');
     const hashed_man = await exports.hash(password);
-    const conn = await db.getPool().getConnection();
+    const conn = await db.getPool();
     const updating = 'update User set auth_token = ? where email = ? and password = ?';
     const updated = await conn.query(updating, [token, email, hashed_man]);
     if (updated[0].affectedRows === 0) {
-        conn.release();
         return 400;
     }
     const authorised = 'select user_id as userId, auth_token as token from User where auth_token = ?';
     const answer = await conn.query(authorised, [token]);
     const return_vals = answer[0][0]
-    conn.release();
     return return_vals;
 };
 
 exports.logout = async function(token) {
-    const conn = await db.getPool().getConnection();
+    const conn = await db.getPool();
     const update = 'update User set auth_token = null where auth_token = ?';
     const updated = await conn.query(update, [token]);;
     if (updated[0].affectedRows == 0) {
-        conn.release();
         return 401;
     }
-    conn.release();
     return updated;
 };
 
 exports.retrieve = async function(user_id, token){
-    const conn = await db.getPool().getConnection();
+    const conn = await db.getPool();
     const exists = 'select * from User where user_id = ?';
     const doesnt = await conn.query(exists, [user_id]);
     if (doesnt[0].length == 0) {
-        conn.release();
         return doesnt[0];
     }
     const select = 'select auth_token from User where user_id = ?';
@@ -67,42 +59,36 @@ exports.retrieve = async function(user_id, token){
     if (checking[0][0].auth_token == token && token != undefined) {
         const check = 'select name, city, country, email from User where user_id = ? and auth_token = ?'
         const checked = await conn.query(check, [user_id, token]);
-        conn.release();
         return checked;
     }
     else {
         const check = 'select name, city, country from User where user_id = ?';
         const checked = await conn.query(check, [user_id]);
-        conn.release();
         return checked;
     }
 };
 
 exports.update = async function(auth_token, user_id, name, email, password, new_password, city, country) {
-    const conn = await db.getPool().getConnection();
+    const conn = await db.getPool();
     const email_test = 'select * from User where email = ?';
     const hashed_man = await exports.hash(password);
     const new_hash = await exports.hash(new_password);
     const test_result = await conn.query(email_test, [email]);
     if (test_result[0].length != 0) {
-        conn.release();
         return 400;
     }
     const select = 'select auth_token from User where user_id = ? and password = ?';
     const checking = await conn.query(select, [user_id, hashed_man]);
     if (checking[0].length == 0) {
-        conn.release();
         return 401;
     }
     if (checking[0][0].auth_token != auth_token) {
-        conn.release();
         return 401;
     }
 
     const correct_user = 'select user_id from User where auth_token = ?';
     const user_result = await conn.query(correct_user, [auth_token]);
     if (user_result[0][0].user_id != user_id) {
-        conn.release();
         return 403;
     }
     else {
@@ -114,44 +100,37 @@ exports.update = async function(auth_token, user_id, name, email, password, new_
         const old_country = result[0][0].country;
         const up_dog = 'update User set name = IfNull(?, ?), password = IfNull(?, ?), email = IfNull(?, ?), city = IfNull(?, ?), country = IfNull(?, ?) where auth_token = ?';
         const updated = await conn.query(up_dog, [name, old_name, new_hash, hashed_man, email, old_email, city, old_city, country, old_country, auth_token]);
-        conn.release();
         return 200;
     }
 };
 
 exports.getPhoto = async function(userId) {
-    const conn = await db.getPool().getConnection();
+    const conn = await db.getPool();
     const file_name = 'select photo_filename from User where user_id = ?';
     const process = await conn.query(file_name, [userId]);
     if (process[0].length == 0) {
-        conn.release();
         return 404;
     }
     const result = process[0][0].photo_filename;
-    conn.release();
     return result;
 };
 
 exports.putPhoto = async function(userId, auth_token, file_name) {
-    const conn = await db.getPool().getConnection();
+    const conn = await db.getPool();
     if (auth_token.length == 0) {
-        conn.release();
         return 401;
     }
     const exists = 'select * from User where user_id = ?';
     const does_it = await conn.query(exists, [userId]);
     if (does_it[0].length == 0) {
-        conn.release();
         return 404;
     }
     const correct_user = 'select user_id from User where auth_token = ?';
     const user_result = await conn.query(correct_user, [auth_token]);
     if (user_result[0].length == 0) {
-        conn.release();
         return 401;
     }
     if (user_result[0][0].user_id != userId) {
-        conn.release();
         return 403;
     }
     else {
@@ -160,34 +139,29 @@ exports.putPhoto = async function(userId, auth_token, file_name) {
         if (does_photo[0][0].photo_filename == null) {
             const replace = 'update User set photo_filename = ? where user_id = ?';
             const replaced = await conn.query(replace, [file_name, userId])
-            conn.release();
             return 201;
         }
         else {
             const create = 'update User set photo_filename = ? where user_id = ?';
             const created = await conn.query(create, [file_name, userId])
-            conn.release();
             return 200;
         }
     }
 };
 
 exports.removePhoto = async function(userId, auth_token) {
-    const conn = await db.getPool().getConnection();
+    const conn = await db.getPool();
     const exists = 'select * from User where user_id = ?';
     const does_it = await conn.query(exists, [userId]);
     if (does_it[0].length == 0) {
-        conn.release();
         return 404;
     }
     const correct_user = 'select user_id from User where auth_token = ?';
     const user_result = await conn.query(correct_user, [auth_token]);
     if (user_result[0].length == 0) {
-        conn.release();
         return 401;
     }
     if (user_result[0][0].user_id != userId) {
-        conn.release();
         return 403;
     }
     else {
@@ -196,7 +170,6 @@ exports.removePhoto = async function(userId, auth_token) {
         const result = file_name[0][0].photo_filename;
         const remove = 'update User set photo_filename = NULL where user_id = ?';
         const removed = await conn.query(remove, [userId])
-        conn.release();
         return result;
     }
 };
