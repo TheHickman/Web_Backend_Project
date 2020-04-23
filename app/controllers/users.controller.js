2536c8c2a37708cf0c04539dc8d84278078cc638
hjh60 <hjh60@uclive.ac.nz>
2020-04-23 20:49:36 +1200

    semeicolon

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -264,7 +264,7 @@ exports.deletePhoto = async function(req, res) {
         }
         else {
             const file_path = path.dirname(require.main.filename) + '/storage/photos/';
-            fs.unlink(file_path + result)
+            fs.unlink(file_path + result);
             res.status(200)
                 .send("OK")
         }
